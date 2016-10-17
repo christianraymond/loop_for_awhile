d09cf827b8d664c1418fefe6b007e1a4df27d7f8
ChristianRaymond <ombenisn.raymond@gmail.com>
2016-10-17 21:54:35 +0200

    testing using assert

--- allFromTown.js
+++ allFromTown.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 function allFromTown(regNumbers){
   if(regNumbers.startsWith('CL')){
     return 'FromStellies';
@@ -6,5 +8,8 @@ function allFromTown(regNumbers){
     return 'FromKuilsriver';
   }
 };
+var FromStellies = allFromTown('CL 123,CY 567,CL 345, CJ 456, CL 341','CL');
+assert.deepEqual(fromStellies, [CL 124, CL 345, CL 341]
 
-console.log(allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'));
+var fromKuilsriver = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CF');
+assert.deepEqual(fromKuilsriver, []);
